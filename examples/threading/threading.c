01e42a3e2e83c6f6c51c72076efa5ea2cad4802e
Mohsin Sardar <mohsin.sardar@seecs.edu.pk>
2023-06-02 08:42:08 +0500

    commit of Assignment 4 part1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -11,9 +13,26 @@
 void* threadfunc(void* thread_param)
 {
 
+    int res;
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    usleep((thread_func_args->wait_to_obtain_ms) * 1000);
+    res = pthread_mutex_lock(thread_func_args->mutex_);
+    if(res != 0)
+    {
+        ERROR_LOG("Fail to lock thread: %s \n", strerror(res));
+        return false;
+    }
+    usleep((thread_func_args->wait_to_release_ms) * 1000);
+    res = pthread_mutex_unlock(thread_func_args->mutex_);
+    if(res != 0)
+    {
+        ERROR_LOG("Fail to unlock thread: %s \n", strerror(res));
+        return false;
+    }
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +47,29 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+     struct thread_data *td1 = (struct thread_data*)  malloc(sizeof (struct thread_data));
+     if(td1 == NULL)
+     {
+         ERROR_LOG("Failed to allocate memory\n");
+	 return false;
+     }
+
+     td1->wait_to_obtain_ms = wait_to_obtain_ms;
+     td1->wait_to_release_ms = wait_to_release_ms;
+     td1->mutex_ = mutex;
+     td1->thread_complete_success = false;
+
+     int res = pthread_create (thread, NULL, threadfunc, (void *) td1);
+     
+     if(res != 0)
+     {
+         ERROR_LOG("Fail to create thread: %s \n", strerror(res));
+	 free(td1);
+	 return false;
+     }
+
+
+    return true;
 }
 
